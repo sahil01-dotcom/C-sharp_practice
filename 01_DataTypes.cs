afb533063f6013268cb004e6e350508b07b29df1
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-25 19:49:33 +0530

    01_DataTypes

---
+++ 01_DataTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Declare variables of different data types//
+
+        // int - Integer data type to store whole numbers
+        // double - Floating point data type to store decimal numbers
+        // string - Sequence of characters to store text
+        // bool - Boolean data type to store true or false values
+        int age = 20;
+        double height = 5.8;
+        string name = "Sahil";
+        char grade = 'A';   
+        bool is_Student = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Student: " + is_Student);
+    }
+}
