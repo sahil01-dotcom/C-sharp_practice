7aeff92c83db02f6fc8c60dd1cf1d1c2e637c995
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-26 23:18:17 +0530

    Pokemon Details using Input_Data

---
+++ 02_Input_Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class  pokemonDetails
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Pokemon: ");
+        string name = Console.ReadLine();
+
+        //(Fire, Water, Ground, Rock, Grass etc.)
+        Console.Write("Pokemon Type: ");
+        string Type = Console.ReadLine();
+
+        Console.Write("Combat Power: ");
+        int power = int.Parse(Console.ReadLine());
+
+        Console.Write("Health Points: ");
+        double health = double.Parse(Console.ReadLine());
+
+        Console.Write("Can This Pokemon Evolve (true/false): ");
+        bool evolve = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n___________Pokemon Details____________ ");
+        Console.WriteLine("Pokemon Name: " + name);
+        Console.WriteLine("Pokemon Type: " + Type);
+        Console.WriteLine("Combat Power: " + power);
+        Console.WriteLine("Health Points: " + health);
+        Console.WriteLine("Can This Pokemon Evolve: " + evolve);
+    }
+}
