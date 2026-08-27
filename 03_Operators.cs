b7a22d0f778af1829f50347b901825adacabb068
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-27 09:43:31 +0530

    Arithmetic Operators

---
+++ 03_Operators.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class operators
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b; // Addition
+        int sub = a - b;// Subtraction
+        int multi = b * a;// Multiplication
+        int mod = a % b;// Module
+        a++;
+        b--;
+
+        Console.WriteLine($"Addition: {sum}");
+        Console.WriteLine($"Subtraction: {sub}");
+        Console.WriteLine($"Multiplication: {multi}");
+        Console.WriteLine($"Modulus: {mod}");
+        Console.WriteLine($"Increment: {a}");
+        Console.WriteLine($"Decrement: {b}");
+     }
+
+ }
