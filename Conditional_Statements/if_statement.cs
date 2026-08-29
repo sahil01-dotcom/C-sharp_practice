4a76c775393c1cb2af7211898984081fbe58e9f7
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-29 19:27:43 +0530

    05_if_statement

---
+++ Conditional_Statements/if_statement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class if_statement
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are eligible to vote.");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible to vote.");
+        }
+    }
+}
