14cb56492c1d2f8b86d41cbbc4fc28ab75936908
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-29 19:20:06 +0530

    04_Multiple Operators

---
+++ 04_MultipleOperators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class MultipleOperators
+ {
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 5;
+
+        Console.WriteLine("num_1 = 10");
+        Console.WriteLine("num_2 = 5");
+
+        //Arithmetic operators:
+        Console.WriteLine($"\n Arithmetic Operators");
+        Console.WriteLine($"Addition: " + (a + b));
+        Console.WriteLine($"Subtraction: "  + (a - b));
+        Console.WriteLine($"Multiplication: " + (a * b));
+        Console.WriteLine($"Division: " + (a / b));
+
+        //Comparison operators : 
+        Console.WriteLine("\n Comparison Operators");
+        Console.WriteLine($"a==b: " + (a == b));
+        Console.WriteLine($"a!=b: "   + (a != b));
+        Console.WriteLine($"a>b: " + (a > b));
+        Console.WriteLine($"a<b: " + (a < b));
+    }
+}
