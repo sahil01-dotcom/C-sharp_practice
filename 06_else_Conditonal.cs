d42dce0d8c2220d3eb7bf0e5dcef97f0c93628a5
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-31 22:12:21 +0530

    06_else_Conditional

---
+++ 06_else_Conditonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class _06_else_if_statement
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Any Number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num %2 == 0)
+         {
+            Console.WriteLine("Even number");
+         }
+        else
+        {
+            Console.WriteLine("Odd number");
+        }
+    }
+
+}
