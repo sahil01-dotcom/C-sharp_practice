3887aa94378fb7d595f0f76c97d051fad62206ab
sahil01-dotcom <sahilmarne8055@gmail.com>
2026-08-31 22:13:35 +0530

    08_switch_statement

---
+++ 08_Swith_statement.cs
@@ -0,0 +1,39 @@
+﻿//week day program using switch statement
+using System;
+public class _08_week_day
+{
+    public static void Main(string[] args)
+    {
+        string dayName;
+        Console.Write("Enter the number of the day (1-7): ");
+        int day = int.Parse(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day number.";
+                break;
+        }Console.Write(dayName);
+    }
+}
